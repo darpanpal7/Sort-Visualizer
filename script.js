473cbb06f3bb0a46ab1a876b5c4da5fda89e7ad8
Darpan <darpanpal7@gmail.com>
2020-03-27 01:15:33 +0530

    Added colors and gap

--- script.js
+++ script.js
@@ -9,8 +9,20 @@ var myCanvas = document.getElementById("Canvas");
 
 var canvasArray ;
 
+var canvasHeight = myCanvas.getAttribute("height");
+var canvasWidth = myCanvas.getAttribute("width");
+
+var colorPallete = {
+    unsorted : "#00a8cc",
+    sorted : "#aa26da",
+    process : "#4dd599",
+    swap : "#fb5b5a",
+    pivot : "#ffdc34"
+};
+
 var canvasContext = myCanvas.getContext("2d");
-canvasContext.fillStyle = "#00bdaa";
+var defaultColor = colorPallete.unsorted;
+canvasContext.fillStyle = defaultColor;
 
 function randomArray(size, limit) {
     var array = new Array(size);
@@ -28,16 +40,14 @@ function initCanvas(){
 
     var size = arraySize.value;
 
-    canvasHeight = myCanvas.getAttribute("height");
-    canvasWidth = myCanvas.getAttribute("width");
-
     canvasContext.clearRect(0,0,canvasWidth,canvasHeight);
     elementWidth = Math.floor(canvasWidth/size);
+    gap = Math.floor((elementWidth/100)*20);
 
     var array = randomArray(size,canvasHeight);
 
     for (i = 0; i < size; i++)
-        canvasContext.fillRect (i*elementWidth,0,elementWidth,array[i]);
+        canvasContext.fillRect (i*elementWidth,0,elementWidth-gap,array[i]);
 
     canvasArray = array;
     return array;
@@ -50,52 +60,56 @@ function swapCanvas(pos1, pos2) {
 
     var size = canvasArray.length;
 
-    canvasHeight = myCanvas.getAttribute("height");
-    canvasWidth = myCanvas.getAttribute("width");
-
     elementWidth = Math.floor(canvasWidth/size);
+    gap = Math.floor((elementWidth/100)*20);
 
     canvasContext.clearRect(pos1*elementWidth,0,elementWidth,canvasHeight);
     canvasContext.clearRect(pos2*elementWidth,0,elementWidth,canvasHeight);
 
-    canvasContext.fillRect (pos1*elementWidth,0,elementWidth,elem2);
-    canvasContext.fillRect (pos2*elementWidth,0,elementWidth,elem1);
+    canvasContext.fillRect (pos1*elementWidth,0,elementWidth-gap,elem2);
+    canvasContext.fillRect (pos2*elementWidth,0,elementWidth-gap,elem1);
 }
 
-function changecolor(pos) {
+function changeColor(pos,state) {
     var elem = canvasArray[pos];
     var size = canvasArray.length;
 
     var size = canvasArray.length;
 
-    canvasHeight = myCanvas.getAttribute("height");
-    canvasWidth = myCanvas.getAttribute("width");
-
     elementWidth = Math.floor(canvasWidth/size);
+    gap = Math.floor((elementWidth/100)*20);
 
-    canvasContext.fillStyle = "#005082";
-    canvasContext.fillRect(pos*elementWidth,0,elementWidth,elem);
-    canvasContext.fillStyle = "#00bdaa";
+    canvasContext.fillStyle = state;
+    canvasContext.fillRect(pos*elementWidth,0,elementWidth-gap,elem);
+    canvasContext.fillStyle = defaultColor;
 }
 
-function revertcolor(pos) {
+function revertColor(pos) {
     var elem = canvasArray[pos];
     var size = canvasArray.length;
 
     var size = canvasArray.length;
 
-    canvasHeight = myCanvas.getAttribute("height");
-    canvasWidth = myCanvas.getAttribute("width");
-
     elementWidth = Math.floor(canvasWidth/size);
+    gap = Math.floor((elementWidth/100)*20);
 
-    canvasContext.fillRect(pos*elementWidth,0,elementWidth,elem);
+    canvasContext.fillRect(pos*elementWidth,0,elementWidth-gap,elem);
 }
 
 function swapArray(pos1, pos2) {
     var temp = canvasArray[pos1];
     canvasArray[pos1] = canvasArray[pos2];
     canvasArray[pos2] = temp;
+}
+
+function colorWhole(state,size) {
+    for (i = 0;i < size; i++)
+        changeColor(i,state);
+}
+
+function revertWhole(size) {
+    for (i = 0;i < size; i++)
+        revertColor(i);
 }
 
 function swap(pos1, pos2) {
@@ -106,39 +120,67 @@ function swap(pos1, pos2) {
 async function bubblesort() {
     var size = canvasArray.length;
     var speed = sortSpeed.value;
-    for(i = 0; i < size-1; i++)
-        for(j = 0; j < size-1-i; j++)
+    for(i = 0; i < size-1; i++){
+        for(j = 0; j < size-1-i; j++){
             if(canvasArray[j]>canvasArray[j+1]){
                 swap(j,j+1);
-                changecolor(j); changecolor(j+1);
+                changeColor(j,colorPallete.swap); changeColor(j+1,colorPallete.swap);
                 await sleep(speed);
-                revertcolor(j); revertcolor(j+1);
+                revertColor(j); revertColor(j+1);
             }
+            else{
+                changeColor(j,colorPallete.process); changeColor(j+1,colorPallete.process);
+                await sleep(speed);
+                revertColor(j); revertColor(j+1);
+            }
+        }
+        changeColor(size-i-1,colorPallete.sorted);
+    }
+    changeColor(0,colorPallete.sorted);
+    await sleep(10*speed);
+    revertWhole(size);
 }
 
 async function selectionsort() {
     var size = canvasArray.length;
     var speed = sortSpeed.value;
-    for(i = 0; i < size; i++)
-        for(j = i+1; j < size; j++)
+    for(i = 0; i < size; i++) {
+        for(j = i+1; j < size; j++) {
             if(canvasArray[i]>canvasArray[j]){
                 swap(i,j);
-                changecolor(j); changecolor(j+1);
+                changeColor(i,colorPallete.swap); changeColor(j,colorPallete.swap);
                 await sleep(speed);
-                revertcolor(j); revertcolor(j+1);
+                revertColor(i); revertColor(j);
             }
+            else{
+                changeColor(i,colorPallete.process); changeColor(j,colorPallete.process);
+                await sleep(speed);
+                revertColor(i); revertColor(j);
+            }
+        }
+        changeColor(i,colorPallete.sorted);
+    }
+    await sleep(10*speed);
+    revertWhole(size);
 }
 
 async function insertionsort() {
     var size = canvasArray.length;
     var speed = sortSpeed.value;
-    for(i = 1; i < size; i++)
+    for(i = 1; i < size; i++){
         for(j = i; j>0&&canvasArray[j] < canvasArray[j-1]; j--){
                 swap(j,j-1);
-                changecolor(j); changecolor(j+1);
+                changeColor(j,colorPallete.swap); changeColor(j-1,colorPallete.swap);
                 await sleep(speed);
-                revertcolor(j); revertcolor(j+1);
+                revertColor(j); revertColor(j-1);
         }
+        changeColor(i,colorPallete.process); changeColor(j,colorPallete.process);
+        await sleep(speed);
+        revertColor(i); revertColor(j);
+    }
+    colorWhole(colorPallete.sorted,size);
+    await sleep(1000);
+    revertWhole(size);
 }
 
 function sort() {
@@ -152,5 +194,5 @@ function sort() {
  }
 
 document.querySelector("body").onload = initCanvas();
-startSort.addEventListener("click" , sort);
+startSort.addEventListener("click", sort);
 newArray.addEventListener("click", initCanvas);
